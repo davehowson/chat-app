85874bc43d616155fdd3d3913fe9221ea675c3c5
Dave Howson <dave.sohan@gmail.com>
2019-06-19 11:53:22 +0530

    Global chat UI implemented

--- client/src/Chat/ChatBox.jsx
+++ client/src/Chat/ChatBox.jsx
@@ -1,22 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
 import SendIcon from '@material-ui/icons/Send';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 
 import {
     useGetGlobalMessages,
     useSendGlobalMessage,
 } from '../Services/chatService';
+import { Avatar } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
     root: {
         padding: theme.spacing(2),
         height: '100%',
     },
-    messageRow: {
+    messageContainer: {
+        height: '100%',
+    },
+    messagesRow: {
+        maxHeight: '70vh',
+        overflowY: 'auto',
+    },
+    newMessageRow: {
+        width: '100%',
+    },
+    inputRow: {
+        display: 'flex',
+        alignItems: 'flex-end',
+    },
+    form: {
         width: '100%',
     },
 }));
@@ -26,10 +45,12 @@ const ChatBox = props => {
     const [messages, setMessages] = useState([]);
     const getGlobalMessages = useGetGlobalMessages();
     const sendGlobalMessage = useSendGlobalMessage();
+    let lastMessage = useRef(null);
     const classes = useStyles();
 
     useEffect(() => {
         reloadMessages();
+        scrollToBottom();
     }, []);
 
     const reloadMessages = () => {
@@ -37,6 +58,12 @@ const ChatBox = props => {
             setMessages(res);
         });
     };
+
+    const scrollToBottom = () => {
+        lastMessage.current.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    useEffect(scrollToBottom, [messages]);
 
     const handleSubmit = e => {
         e.preventDefault();
@@ -52,41 +79,50 @@ const ChatBox = props => {
                 <Typography variant="h6">{props.scope}</Typography>
             </Grid>
             <Grid item xs={12}>
-                <Grid container>
-                    <Grid item xs={12}>
+                <Grid container className={classes.messageContainer}>
+                    <Grid item xs={12} className={classes.messagesRow}>
                         {messages && (
-                            <>
-                                <ul>
-                                    {messages.map(x => (
-                                        <li key={x._id}>{x.body}</li>
-                                    ))}
-                                </ul>
-                            </>
+                            <List>
+                                {messages.map(m => (
+                                    <ListItem key={m._id}>
+                                        <ListItemAvatar>
+                                            <Avatar>H</Avatar>
+                                        </ListItemAvatar>
+                                        <ListItemText
+                                            primary="Chad Bradley"
+                                            secondary={
+                                                <React.Fragment>
+                                                    {m.body}
+                                                </React.Fragment>
+                                            }
+                                        />
+                                    </ListItem>
+                                ))}
+                            </List>
                         )}
+                        <div ref={lastMessage} />
                     </Grid>
-                    <Grid item xs={12}>
-                        <form onSubmit={handleSubmit}>
+                    <Grid item xs={12} className={classes.inputRow}>
+                        <form onSubmit={handleSubmit} className={classes.form}>
                             <Grid
                                 container
-                                className={classes.messageRow}
+                                className={classes.newMessageRow}
                                 alignItems="flex-end"
                             >
-                                <Grid item xs={10}>
+                                <Grid item xs={11}>
                                     <TextField
                                         id="message"
                                         label="Message"
                                         variant="outlined"
                                         margin="dense"
                                         fullWidth
-                                        multiline
-                                        rowsMax={4}
                                         value={newMessage}
                                         onChange={e =>
                                             setNewMessage(e.target.value)
                                         }
                                     />
                                 </Grid>
-                                <Grid item xs={2}>
+                                <Grid item xs={1}>
                                     <IconButton type="submit">
                                         <SendIcon />
                                     </IconButton>
