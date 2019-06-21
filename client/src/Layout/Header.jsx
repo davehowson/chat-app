4cb35abe50cf5d2a21aea5897955bc1ffa7e1672
Dave Howson <dave.sohan@gmail.com>
2019-06-21 08:57:39 +0530

    Header redesign

--- client/src/Layout/Header.jsx
+++ client/src/Layout/Header.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import Link from '@material-ui/core/Link';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 
 import { authenticationService } from '../Services/authenticationService';
 import history from '../Utilities/history';
@@ -16,13 +19,44 @@ const useStyles = makeStyles(theme => ({
     },
     title: {
         flexGrow: 1,
+        display: 'flex',
+    },
+    userDropdown: {
+        marginLeft: theme.spacing(2),
+        padding: theme.spacing(1),
+        [theme.breakpoints.down('xs')]: {
+            marginLeft: 'auto',
+        },
     },
 }));
 
 const Header = () => {
+    const [currentUser, setCurrentUser] = useState(
+        authenticationService.currentUserValue
+    );
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const handleDropClose = () => {
+        setDropdownOpen(false);
+        setAnchorEl(null);
+    };
+
+    const handleDropOpen = event => {
+        setDropdownOpen(true);
+        setAnchorEl(event.currentTarget);
+    };
+
     const handleLogout = () => {
         authenticationService.logout();
         history.push('/');
+    };
+
+    const arrowIcon = () => {
+        if (dropdownOpen) {
+            return <ArrowDropUpIcon />;
+        }
+        return <ArrowDropDownIcon />;
     };
 
     const classes = useStyles();
@@ -34,9 +68,33 @@ const Header = () => {
                     <Link href="/" className={classes.title}>
                         <img src={logo} alt="Logo" />
                     </Link>
-                    <Button color="inherit" onClick={handleLogout}>
-                        Logout
+                    <Button
+                        aria-owns={anchorEl ? 'simple-menu' : undefined}
+                        aria-haspopup="true"
+                        onClick={handleDropOpen}
+                        className={classes.userDropdown}
+                        color="inherit"
+                    >
+                        {currentUser.name}
+                        {arrowIcon()}
                     </Button>
+                    <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={handleDropClose}
+                        getContentAnchorEl={null}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'right',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                    >
+                        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                    </Menu>
                 </Toolbar>
             </AppBar>
         </div>
