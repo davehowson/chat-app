f4c9679b4ba285f02e51d668bb3d8cfa646f3665
Dave Howson <dave.sohan@gmail.com>
2020-10-14 22:29:18 +0530

    Multiple package updates

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI:
-        'mongodb+srv://user:9w7HVR6z77UujXYQ@cluster0-n4wm7.gcp.mongodb.net/test?retryWrites=true&w=majority',
-    secretOrKey: 'secret',
-};
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const cors = require("cors");
 
-const users = require('./routes/api/users');
-const messages = require('./routes/api/messages');
+const users = require("./routes/api/users");
+const messages = require("./routes/api/messages");
 
 const app = express();
 
@@ -13,16 +13,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () =>
-    console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 );
 
-const io = require('socket.io').listen(server);
+const io = require("socket.io").listen(server);
 
 // Body Parser middleware to parse request bodies
 app.use(
-    bodyParser.urlencoded({
-        extended: false,
-    })
+  bodyParser.urlencoded({
+    extended: false,
+  })
 );
 app.use(bodyParser.json());
 
@@ -30,24 +30,28 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Database configuration
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 mongoose
-    .connect(db, { useNewUrlParser: true, useFindAndModify: false })
-    .then(() => console.log('MongoDB Successfully Connected'))
-    .catch(err => console.log(err));
+  .connect(db, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Successfully Connected"))
+  .catch((err) => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
 // Passport config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Assign socket object to every request
-app.use(function(req, res, next) {
-    req.io = io;
-    next();
+app.use(function (req, res, next) {
+  req.io = io;
+  next();
 });
 
 // Routes
-app.use('/api/users', users);
-app.use('/api/messages', messages);
+app.use("/api/users", users);
+app.use("/api/messages", messages);
